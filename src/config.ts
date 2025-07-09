9171e010ada29f1227defbd0bc37b0debc341173
Lukas Januskevicius <lukasjanuskevicius@MacBookPro.lan>
2025-07-09 14:32:01 +0300

    update: .env, config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,10 @@ import z from 'zod';
 
 const schema = z.object({
   jamendoClientId: z.string(),
-  jamendoApiKey: z.string(),
 });
 
 const config = schema.parse({
   jamendoClientId: import.meta.env.VITE_JAMENDO_CLIENT_ID,
-  jamendoApiKey: import.meta.env.VITE_JAMENDO_API_KEY,
 });
 
 export default config;
